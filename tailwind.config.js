44703563a1b15b62208f39f91408dadd8f443496
Tim Remnant <timwebdesign2013@gmail.com>
2021-03-23 21:32:38 +0000

    Adding config files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: ['./src/**/*.tsx', './public/index.html'],
+    darkMode: 'class',
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {
+        opacity: ['disabled']
+      },
+    },
+    plugins: [],
+}
