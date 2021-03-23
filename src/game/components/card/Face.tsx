cfe97e130936ec93755f98babf4e1e4e9119d5d9
Tim Remnant <timwebdesign2013@gmail.com>
2021-03-23 22:40:37 +0000

    Can now build card faces

---
+++ src/game/components/card/Face.tsx
@@ -0,0 +1,27 @@
+import { resolveLargeSuitIcon, resolveSmallSuitIcon } from "./SuitBuilder";
+
+interface FaceProps {
+    index: string;
+    type: string;
+}
+export const Face = ({index, type}: FaceProps): JSX.Element => {
+    
+    const small = resolveSmallSuitIcon(type);
+    const large = resolveLargeSuitIcon(type);
+    
+    return (
+        <div className="relative border-2 border-black rounded-md w-40 h-60 shadow-md flex flex-col justify-center">
+            <div className="absolute top-0 left-0 p-2 m-1">
+                <p className="text-center">{index}</p>
+                {small}
+            </div>
+            <div className="flex justify-center">
+                {large}
+            </div>
+            <div className="absolute bottom-0 right-0 p-2 m-1">
+                {small}
+                <p className="text-center transform-gpu rotate-180">{index}</p>
+            </div>
+        </div>
+    );
+};
---
+++ src/game/components/card/SuitBuilder.tsx
@@ -0,0 +1,55 @@
+import { SUIT_TYPE_CLUB, SUIT_TYPE_DIAMOND, SUIT_TYPE_HEART, SUIT_TYPE_SPADE } from "../../../store/game/suitTypes";
+import { Club, Diamond, Spade, Heart } from "./Suit";
+
+const smallWidth: number = 20;
+const smallHeight: number = 20;
+
+const largeWidth: number = 80;
+const largeHeight: number = 80;
+
+export const resolveSmallSuitIcon = (type: string): JSX.Element => {
+
+    let icon: JSX.Element | null = null;
+
+    switch (type) {
+        case SUIT_TYPE_SPADE:
+            icon = <Spade width={smallWidth} height={smallHeight}/>;
+            break;
+        case SUIT_TYPE_CLUB:
+            icon = <Club width={smallWidth} height={smallHeight}/>
+            break;
+        case SUIT_TYPE_HEART:
+            icon = <Heart width={smallWidth} height={smallHeight}/>
+            break;
+        case SUIT_TYPE_DIAMOND:
+            icon = <Diamond width={smallWidth} height={smallHeight}/>
+            break;
+        default:
+            throw new Error('No icon found for ' + type);
+    }
+    return icon;
+};
+
+
+export const resolveLargeSuitIcon = (type: string): JSX.Element => {
+
+    let icon: JSX.Element | null = null;
+
+    switch (type) {
+        case SUIT_TYPE_SPADE:
+            icon = <Spade width={largeWidth} height={largeHeight}/>;
+            break;
+        case SUIT_TYPE_CLUB:
+            icon = <Club width={largeWidth} height={largeHeight}/>
+            break;
+        case SUIT_TYPE_HEART:
+            icon = <Heart width={largeWidth} height={largeHeight}/>
+            break;
+        case SUIT_TYPE_DIAMOND:
+            icon = <Diamond width={largeWidth} height={largeHeight}/>
+            break;
+        default:
+            throw new Error('No icon found for ' + type);
+    }
+    return icon;
+};
