aec199aedf0c073357554977270d0745b868e6cf
Tim Remnant <timwebdesign2013@gmail.com>
2021-03-23 22:40:48 +0000

    Default game route

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import {
   Route
 } from 'react-router-dom';
 
+import { Game } from './game/index';
+
 export const App = (): JSX.Element => {
   return (
     <main className="font-sans antialiased leading-normal tracking-wider bg-gray-100 dark:bg-gray-700 dark:text-white">
       <BrowserRouter>
         <Switch>
+          <Route path="/">
+            <Game/>
+          </Route>
         </Switch>
       </BrowserRouter>
     </main>
