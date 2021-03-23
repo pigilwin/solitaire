ec6678abbaa0cb34e8fe2e26d24ee4c2324d556b
Tim Remnant <timwebdesign2013@gmail.com>
2021-03-23 21:54:27 +0000

    Adding suit svgs

---
+++ src/game/components/card/Suit.tsx
@@ -0,0 +1,57 @@
+export const Heart = (): JSX.Element => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" baseProfile="tiny" viewBox="0 0 600 600">
+            <g transform="rotate(45,300,300)">
+                <rect x="150" y="150" height="350" width="350" />
+                <circle cx="150" cy="325" r="175" />
+                <circle cx="325" cy="150" r="175" />
+            </g>
+        </svg>
+    );
+};
+
+export const Diamond = (): JSX.Element => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" baseProfile="tiny" viewBox="0 0 600 600">
+            <rect x="100" y="100" width="400" height="400" transform="rotate(45,300,300)" />
+        </svg>
+    );
+};
+
+export const Spade = (): JSX.Element => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" baseProfile="tiny" viewBox="0 0 600 600">
+            <defs>
+                <mask id="stem-mask">
+                    <rect width="100%" height="100%" fill="#fff"/>
+                    <circle cx="180" cy="490" r="100" fill="#000" />
+                    <circle cx="420" cy="490" r="100" fill="#000" />
+                </mask>
+            </defs>
+            <rect x="200" y="400" height="200" width="200" fill="#000" mask="url(#stem-mask)" />
+            <g transform="rotate(225,300,300)">
+                <rect width="300" height="300" x="200" y="200" />
+                <circle cx="200" cy="350" r="150" />
+                <circle cx="350" cy="200" r="150" />
+            </g>
+        </svg>
+    );
+};
+
+export const Club = (): JSX.Element => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" baseProfile="tiny" viewBox="0 0 600 600">
+            <defs>
+                <mask id="stem-mask">
+                    <rect width="100%" height="100%" fill="#fff"/>
+                    <circle cx="180" cy="490" r="100" fill="#000" />
+                    <circle cx="420" cy="490" r="100" fill="#000" />
+                </mask>
+            </defs>
+            <rect x="200" y="400" height="200" width="200" fill="#000" mask="url(#stem-mask)" />
+            <circle cx="180" cy="350" r="140" />
+            <circle cx="300" cy="150" r="140" />
+            <circle cx="420" cy="350" r="140" />
+        </svg>
+    );
+};
