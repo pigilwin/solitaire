3bd127b239fb0600aa5c47221dcb079c616ddc96
Tim Remnant <timwebdesign2013@gmail.com>
2021-03-24 22:47:39 +0000

    Removing games reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import { reducer as gameReducer } from './game/gameSlice';
-import { reducer as gamesReducer } from './game/gamesSlice';
 
 
 export const rootReducer = combineReducers({
-    gameReducer,
-    gamesReducer
+    gameReducer
 });
 export type RootState = ReturnType<typeof rootReducer>;
 export type RootStateHook = () => RootState;
