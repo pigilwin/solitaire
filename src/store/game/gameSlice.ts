e8a9c12396eacceea5d6ab5fdfca7f0ce02f51b1
Tim Remnant <timwebdesign2013@gmail.com>
2021-03-24 23:02:53 +0000

    Adding initial state

--- src/store/game/gameSlice.ts
+++ src/store/game/gameSlice.ts
@@ -1,7 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { Solitaire } from './suitTypes';
 
-export const initialState =  {
-
+export const initialState: Solitaire =  {
+    id: '',
+    score: 0,
+    start: Date.now(),
+    end: Date.now(),
+    first: [],
+    second: [],
+    third: [],
+    fourth: [],
+    five: [],
+    sixth: [],
+    seventh: []
 };
 
 const gameSlice = createSlice({
--- src/store/game/suitTypes.ts
+++ src/store/game/suitTypes.ts
@@ -2,3 +2,24 @@ export const SUIT_TYPE_SPADE: string = 'SPADE';
 export const SUIT_TYPE_HEART: string = 'HEART';
 export const SUIT_TYPE_DIAMOND: string = 'DIAMOND';
 export const SUIT_TYPE_CLUB: string = 'CLUB';
+
+export type SUIT = typeof SUIT_TYPE_SPADE | typeof SUIT_TYPE_CLUB | typeof SUIT_TYPE_DIAMOND | typeof SUIT_TYPE_HEART;
+export interface Solitaire {
+    id: string;
+    score: number;
+    start: number;
+    end: number;
+    first: SolitaireCard[];
+    second: SolitaireCard[];
+    third: SolitaireCard[];
+    fourth: SolitaireCard[];
+    five: SolitaireCard[];
+    sixth: SolitaireCard[];
+    seventh: SolitaireCard[];
+}
+
+export interface SolitaireCard {
+    suit: SUIT;
+    index: string;
+    showing: boolean;
+}
