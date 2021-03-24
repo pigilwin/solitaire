43b7fa1e6668829632286ea35b9b433bece3fe1f
Tim Remnant <timwebdesign2013@gmail.com>
2021-03-24 22:43:29 +0000

    Adding new game, leaderboard and settings to gamebar

--- src/game/components/GameBar.tsx
+++ src/game/components/GameBar.tsx
@@ -1,10 +1,23 @@
-import { GameButton } from "./Button"
+import { GameButton } from "./Button";
 
 export const GameBar = (): JSX.Element => {
     return (
         <nav className="w-full bg-white shadow-lg border-b border-green-500 h-16">
-            <div className="w-full flex flex-row justify-end p-2">
-                <GameButton buttonText="New Game"/>
+            <div className="container mx-auto px-6 py-3 flex justify-between items-center">
+                <div className="flex justify-between items-center">
+                    <h1 className="text-gray-800 font-bold text-2xl hover:text-gray-700 cursor-pointer">Solitaire</h1>
+                </div>
+                <div className="flex flex-row">
+                    <div className="px-1">
+                        <GameButton buttonText="New Game"/>
+                    </div>
+                    <div className="px-1">
+                        <GameButton buttonText="Leaderboard"/>
+                    </div>
+                    <div className="px-1">
+                        <GameButton buttonText="Settings"/>
+                    </div>
+                </div>
             </div>
         </nav>
     );
