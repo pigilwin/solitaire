b778040df2e8a8c802aa60af1bb8c07827a31921
Tim Remnant <timwebdesign2013@gmail.com>
2021-03-24 23:47:22 +0000

    Adding click event handler

--- src/game/components/Button.tsx
+++ src/game/components/Button.tsx
@@ -1,13 +1,17 @@
+import { MouseEventHandler } from "react";
+
 interface GameButtonProps {
     buttonText: string;
     disabled?: boolean;
+    onClick: MouseEventHandler<HTMLButtonElement>;
 }
 
-export const GameButton = ({buttonText, disabled}: GameButtonProps): JSX.Element => {
+export const GameButton = ({buttonText, disabled, onClick}: GameButtonProps): JSX.Element => {
     return (
         <button
             disabled={disabled}
             className="bg-blue-600 text-gray-200 p-2 rounded hover:bg-blue-500 hover:text-gray-100"
+            onClick={onClick}
         >{buttonText}</button>
     );
 };
