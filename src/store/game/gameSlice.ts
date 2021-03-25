4347d5b55dae24aec0e77dcda52cbde277b0ddb0
Tim Remnant <timwebdesign2013@gmail.com>
2021-03-25 19:51:23 +0000

    Fetching current game with selector

--- src/store/game/gameSlice.ts
+++ src/store/game/gameSlice.ts
@@ -1,24 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../rootReducer';
 import { generateGame } from './game';
-import { Solitaire } from './suitTypes';
+import { Game, Solitaire } from './suitTypes';
 
-export const initialState: Solitaire =  {
-    id: '',
-    score: 0,
-    start: Date.now(),
-    end: Date.now(),
-    first: [],
-    second: [],
-    third: [],
-    fourth: [],
-    five: [],
-    sixth: [],
-    seventh: [],
-    heart: [],
-    diamond: [],
-    spade: [],
-    club: [],
-    draw: []
+export const initialState: Game =  {
+    game: {
+        id: '',
+        score: 0,
+        start: Date.now(),
+        end: Date.now(),
+        first: [],
+        second: [],
+        third: [],
+        fourth: [],
+        five: [],
+        sixth: [],
+        seventh: [],
+        heart: [],
+        diamond: [],
+        spade: [],
+        club: [],
+        draw: []
+    }
 };
 
 const gameSlice = createSlice({
@@ -26,7 +29,9 @@ const gameSlice = createSlice({
     initialState,
     reducers: {
         initialiseGame(state) {
-            return generateGame();
+            const newGame = state;
+            newGame.game = generateGame();
+            return newGame;
         }
     }
 });
@@ -35,3 +40,5 @@ export const reducer = gameSlice.reducer;
 export const {
     initialiseGame
 } = gameSlice.actions;
+
+export const currentGameSelector = (state: RootState): Solitaire => state.gameReducer.game;
--- src/store/game/suitTypes.ts
+++ src/store/game/suitTypes.ts
@@ -4,6 +4,11 @@ export const SUIT_TYPE_DIAMOND: string = 'DIAMOND';
 export const SUIT_TYPE_CLUB: string = 'CLUB';
 
 export type SUIT = typeof SUIT_TYPE_SPADE | typeof SUIT_TYPE_CLUB | typeof SUIT_TYPE_DIAMOND | typeof SUIT_TYPE_HEART;
+
+export interface Game {
+    game: Solitaire;
+}
+
 export interface Solitaire {
     id: string;
     score: number;
