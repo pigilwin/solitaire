7089dc07335410c26f097bb899fe4b81bdb81c32
Tim Remnant <timwebdesign2013@gmail.com>
2021-03-25 16:12:07 +0000

    Adding board to contain the game

---
+++ src/game/board.tsx
@@ -0,0 +1,9 @@
+import { TopBar } from "./components/board/TopBar";
+
+export const Board = (): JSX.Element => {
+    return (
+        <div id="board">
+            <TopBar/>
+        </div>
+    );
+};
