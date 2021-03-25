1942359725a1e4083d2df7d23a4b250fd453f758
Tim Remnant <timwebdesign2013@gmail.com>
2021-03-25 16:12:25 +0000

    Adding the board wrapped in a drag and drop context

--- src/game/index.tsx
+++ src/game/index.tsx
@@ -1,9 +1,19 @@
+import { DragDropContext, DropResult, ResponderProvided } from "react-beautiful-dnd"
 import { GameBar } from "./components/GameBar"
+import { Board } from "./board";
 
 export const Game = (): JSX.Element => {
+
+    const onDragEndHandler = (result: DropResult, provided: ResponderProvided) => {
+
+    };
+
     return (
-        <div className="mx-auto min-h-screen container">
+        <div className="min-h-screen bg-green-300">
             <GameBar/>
+            <DragDropContext onDragEnd={onDragEndHandler}>
+                <Board/>
+            </DragDropContext>
         </div>
     );
 };
