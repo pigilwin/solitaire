ad8a32ae6c2233fead5c214616ae7fe7b1cc70ad
Tim Remnant <timwebdesign2013@gmail.com>
2021-03-26 23:40:13 +0000

    Adding droppable to empty card space

--- src/game/components/board/EmptyCardSpace.tsx
+++ src/game/components/board/EmptyCardSpace.tsx
@@ -1,5 +1,23 @@
+import { useDrop } from "react-dnd";
+
 export const EmptyCardSpace = (): JSX.Element => {
+    
+    /**
+     * CollectionOptions is not used but is now disabled
+     */
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [CollectionOptions, drop] = useDrop(() => ({
+        accept: 'card',
+        drop: (i, monitor) => {
+            console.log(i, monitor);
+        },
+        canDrop: (i, monitor) => {
+            console.log(i, monitor);
+            return true;
+        }
+    }), []);
+    
     return (
-        <div className="playing-card-container"></div>
+        <div ref={drop} className="playing-card-container"></div>
     );
 }
