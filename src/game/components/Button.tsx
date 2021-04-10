c8920d845974d5f4b04162206fcefb14cb08daad
Tim Remnant <timwebdesign2013@gmail.com>
2021-04-10 16:54:02 +0100

    Adding icon game button that acts like a button but allows an icon

--- src/game/components/Button.tsx
+++ src/game/components/Button.tsx
@@ -15,3 +15,20 @@ export const GameButton = ({buttonText, disabled, onClick}: GameButtonProps): JS
         >{buttonText}</button>
     );
 };
+
+interface IconGameButtonProps extends GameButtonProps {
+    icon: JSX.Element;
+}
+
+export const IconGameButton = ({buttonText, disabled, onClick, icon}: IconGameButtonProps): JSX.Element => {
+    return (
+        <button
+            disabled={disabled}
+            className="bg-blue-600 text-gray-200 p-2 rounded hover:bg-blue-500 hover:text-gray-100 inline-flex items-center justify-around"
+            onClick={onClick}
+        >
+            {icon}
+            <span>{buttonText}</span>
+        </button>
+    );
+}
