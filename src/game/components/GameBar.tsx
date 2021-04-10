ae8ecaabdc36580afa59ca35d8fc680a42fbf5fb
Tim Remnant <timwebdesign2013@gmail.com>
2021-04-10 16:58:40 +0100

    As the game bar has become more complex, it has been split apart

--- src/game/components/GameBar.tsx
+++ src/game/components/GameBar.tsx
@@ -1,36 +1,81 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { replaceGameAction } from "../../store/game/gameSlice";
 import { initialiseGameAsync } from "../../store/game/thunk";
-import { GameButton } from "./Button";
+import { doWeHaveAnyHistorySelector, latestHistoryItemSelector, removeLatestHistoryItemAction } from "../../store/history/historySlice";
+import { GameButton, IconGameButton } from "./Button";
+import { Undo } from "./Icons";
 
 export const GameBar = (): JSX.Element => {
     
+    let undoButton: JSX.Element | null = null;
+    const weHaveHistory = useSelector(doWeHaveAnyHistorySelector);
+    
+    if (weHaveHistory) {
+        undoButton = <UndoGameButton/>;
+    }
+
+    return (
+        <nav className="w-full bg-white shadow-lg border-b border-green-500 h-16">
+            <div className="container mx-auto px-6 py-3 flex justify-between items-center">
+                <GameLogo/>
+                {undoButton}
+                <GameButtons/>
+            </div>
+        </nav>
+    );
+};
+
+const GameLogo = (): JSX.Element => {
+    return (
+        <div className="flex flex-row">
+            <div className="flex justify-between items-center">
+                <h1 className="text-gray-800 font-bold text-2xl hover:text-gray-700 cursor-pointer">Solitaire</h1>
+            </div>      
+        </div>
+    );
+}
+
+const UndoGameButton = (): JSX.Element => {
+    const dispatch = useDispatch();
+    const game = useSelector(latestHistoryItemSelector);
+    const undoHistoryClickHandler = () => {
+        dispatch(removeLatestHistoryItemAction());
+        dispatch(replaceGameAction(game));
+    };
+    
+    return (
+        <div className="flex flex-row">    
+            <IconGameButton
+                buttonText="Undo"
+                icon={<Undo/>}
+                onClick={undoHistoryClickHandler}
+            />
+        </div>
+    );
+}
+
+const GameButtons = (): JSX.Element => {
+
     const dispatch = useDispatch();
 
     const newGameClickHandler = () => {
         dispatch(initialiseGameAsync());
     };
+
     const leaderboardClickHandler = () => {};
     const settingsClickHandler = () => {};
-    
-    
+
     return (
-        <nav className="w-full bg-white shadow-lg border-b border-green-500 h-16">
-            <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-                <div className="flex justify-between items-center">
-                    <h1 className="text-gray-800 font-bold text-2xl hover:text-gray-700 cursor-pointer">Solitaire</h1>
-                </div>
-                <div className="flex flex-row">
-                    <div className="px-1">
-                        <GameButton buttonText="New Game" onClick={newGameClickHandler}/>
-                    </div>
-                    <div className="px-1">
-                        <GameButton buttonText="Leaderboard" onClick={leaderboardClickHandler}/>
-                    </div>
-                    <div className="px-1">
-                        <GameButton buttonText="Settings" onClick={settingsClickHandler}/>
-                    </div>
-                </div>
+        <div className="flex flex-row">
+            <div className="px-1">
+                <GameButton buttonText="New Game" onClick={newGameClickHandler}/>
             </div>
-        </nav>
+            <div className="px-1">
+                <GameButton buttonText="Leaderboard" onClick={leaderboardClickHandler}/>
+            </div>
+            <div className="px-1">
+                <GameButton buttonText="Settings" onClick={settingsClickHandler}/>
+            </div>
+        </div>
     );
-};
+}
