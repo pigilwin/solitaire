0179df34b34971d6c196a8eef67cd0609ec3bf9e
Tim Remnant <timwebdesign2013@gmail.com>
2021-04-17 00:42:55 +0100

    Show the testing routes button if the logo is clicked 7 times

--- src/game/components/bar/GameLogo.tsx
+++ src/game/components/bar/GameLogo.tsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { allowedToSeeTestingRouteAction } from "../../../store/application/applicationSlice";
+import { countToRevealTestingRoute } from "../../../store/application/constants";
+
 export const GameLogo = (): JSX.Element => {
+
+    const dispatch = useDispatch();
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        if (count === countToRevealTestingRoute) {
+            dispatch(allowedToSeeTestingRouteAction(true));
+            setCount(0);
+        }
+    }, [dispatch, count]);
+
+    const onClickHandler = () => {
+        setCount(count + 1);
+    };
+
     return (
         <div className="flex flex-row">
             <div className="flex justify-between items-center">
-                <h1 className="text-gray-800 font-bold text-2xl hover:text-gray-700 cursor-pointer">Solitaire</h1>
+                <h1 className="text-gray-800 font-bold text-2xl hover:text-gray-700 cursor-pointer" onClick={onClickHandler}>Solitaire</h1>
             </div>      
         </div>
     );
---
+++ src/store/application/constants.ts
@@ -0,0 +1,1 @@
+export const countToRevealTestingRoute: number = 7;
