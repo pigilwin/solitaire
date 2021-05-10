3fbb382aa08abb4918b01ea4905d394c6b2e8b7c
Tim Remnant <timwebdesign2013@gmail.com>
2021-05-11 00:22:08 +0100

    Card back can now be selected

--- src/game/components/board/card/Back.tsx
+++ src/game/components/board/card/Back.tsx
@@ -1,7 +1,43 @@
 export const Back = (): JSX.Element => {    
+    return <Blue/>;
+};
+
+export const Green = (): JSX.Element => {
     return (
-        <div className="playing-card bg-green-200 p-1">
-            <div className="border-2 border-white border-dotted w-28 h-40 bg-green-500 mx-auto"></div>
+        <div className="playing-card p-1">
+            <div className="border-2 border-white border-dotted playing-card-back-inner bg-green-500 mx-auto"></div>
         </div>
     );
-};
+}
+
+export const Blue = (): JSX.Element => {
+    return (
+        <div className="playing-card p-1">
+            <div className="border-2 border-white border-dashed playing-card-back-inner bg-blue-500 mx-auto"></div>
+        </div>
+    );
+}
+
+export const Red = (): JSX.Element => {
+    return (
+        <div className="playing-card p-1">
+            <div className="border-2 border-white border-dotted playing-card-back-inner bg-red-500 mx-auto"></div>
+        </div>
+    );
+}
+
+export const Yellow = (): JSX.Element => {
+    return (
+        <div className="playing-card p-1">
+            <div className="border-2 border-white border-dotted playing-card-back-inner bg-yellow-500 mx-auto"></div>
+        </div>
+    );
+}
+
+export const Purple = (): JSX.Element => {
+    return (
+        <div className="playing-card p-1">
+            <div className="border-2 border-white border-dotted playing-card-back-inner bg-purple-500 mx-auto"></div>
+        </div>
+    );
+}
--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -1,12 +1,39 @@
 import { useHistory } from "react-router-dom";
-import {GameButton } from './components/Button';
+import { GameButton } from './components/Button';
 import { Accordion } from "./layout/Accordion";
 import { FullPageContainer } from "./layout/FullPageContainer";
+
+import {
+    Blue,
+    Red,
+    Green,
+    Purple,
+    Yellow,
+} from './components/board/card/Back';
+import { 
+    CardBackMap,
+    BACK_BLUE,
+    BACK_GREEN,
+    BACK_RED,
+    BACK_YELLOW,
+    BACK_PURPLE
+} from "types/back";
+import { PropsWithChildren } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { currentlySelectedCardBackSelector, applyNewCardBackAction } from "store/application/applicationSlice";
 
 export const Settings = (): JSX.Element => {
     const history = useHistory();
     const onClickHandler = () => {
         history.replace('/');
+    };
+
+    const cardBacks: CardBackMap = {
+        [BACK_BLUE]: (<Blue/>),
+        [BACK_GREEN]: (<Green/>),
+        [BACK_PURPLE]: (<Purple/>),
+        [BACK_RED]: (<Red/>),
+        [BACK_YELLOW]: (<Yellow/>)
     };
 
     return (
@@ -18,10 +45,40 @@ export const Settings = (): JSX.Element => {
                 </div>
                 <div className="mx-1">
                     <Accordion title="Choose a new card back">
-
+                        <div className="grid grid-cols-3 gap-4">
+                            {Object.keys(cardBacks).map((id) => {
+                                return (
+                                   <SelectableCardBack key={id} id={id}>
+                                       {cardBacks[id]}
+                                   </SelectableCardBack>
+                                );
+                            })}
+                        </div>
                     </Accordion>
                 </div>
             </div>
         </FullPageContainer>
+    );
+}
+
+interface SelectableCardBackProps {
+    id: string;
+}
+const SelectableCardBack = ({id, children}: PropsWithChildren<SelectableCardBackProps>): JSX.Element => {
+    
+    const dispatch = useDispatch();
+    const onClickHandler = () => {
+        dispatch(applyNewCardBackAction(id));
+    };
+
+    const classes = ['w-36', 'h-46', 'mx-auto', 'cursor-pointer'];
+    if (useSelector(currentlySelectedCardBackSelector) === id) {
+        classes.push('border-8', 'border-blue-200');
+    }
+
+    return (
+        <div className={classes.join(' ')} onClick={onClickHandler}>
+            {children}
+        </div>
     );
 }
