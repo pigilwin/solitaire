91d9c29d6165fd83f10a2d3d1748f1f10f6bfd31
Tim Remnant <timwebdesign2013@gmail.com>
2021-05-10 23:07:52 +0100

    Swapping to full page container

--- src/game/settings.tsx
+++ src/game/settings.tsx
@@ -1,6 +1,7 @@
 import { useHistory } from "react-router-dom";
 import {GameButton } from './components/Button';
 import { Accordion } from "./layout/Accordion";
+import { FullPageContainer } from "./layout/FullPageContainer";
 
 export const Settings = (): JSX.Element => {
     const history = useHistory();
@@ -9,7 +10,7 @@ export const Settings = (): JSX.Element => {
     };
 
     return (
-        <div className="flex min-h-screen bg-green-300">
+        <FullPageContainer>
             <div className="m-auto w-1/2 bg-white flex flex-col justify-between space-y-4 rounded-md">
                 <h1 className="text-center text-4xl mt-4">Settings</h1>
                 <div className="my-2 text-center">
@@ -21,6 +22,6 @@ export const Settings = (): JSX.Element => {
                     </Accordion>
                 </div>
             </div>
-        </div>
+        </FullPageContainer>
     );
 }
