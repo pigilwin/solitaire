a132e681f0a9877c0e8b496f455d2750c259d82f
Tim Remnant <timwebdesign2013@gmail.com>
2021-05-11 22:37:01 +0100

    Reloading the back

--- src/game/components/board/card/Back.tsx
+++ src/game/components/board/card/Back.tsx
@@ -1,5 +1,30 @@
+import { useSelector } from "react-redux";
+import { currentlySelectedCardBackSelector } from "store/application/applicationSlice";
+import { 
+    BACK_BLUE, 
+    BACK_GREEN,
+    BACK_YELLOW,
+    BACK_PURPLE,
+    BACK_RED
+} from "types/back";
+
 export const Back = (): JSX.Element => {    
-    return <Blue/>;
+    const color = useSelector(currentlySelectedCardBackSelector);
+
+    switch (color) {
+        case BACK_GREEN:
+            return <Green/>;
+        case BACK_BLUE:
+            return <Blue/>;
+        case BACK_RED:
+            return <Red/>;
+        case BACK_PURPLE:
+            return <Purple/>;
+        case BACK_YELLOW:
+            return <Yellow/>;
+    }
+
+    throw new Error('Invalid color - ' + color);
 };
 
 export const Green = (): JSX.Element => {
