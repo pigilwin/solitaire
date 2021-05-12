2b59ab972f62b4bebecc05610f22ed4095dca5a9
Tim Remnant <timwebdesign2013@gmail.com>
2021-05-12 22:23:54 +0100

    Now terminating the workers when they are complete. When workers aren't terminated they will leak memory, this will allow a worker to be created, the work done and then closed

--- src/invokeWorkers.ts
+++ src/invokeWorkers.ts
@@ -1,14 +1,20 @@
-import { wrap } from 'comlink';
+import { wrap, releaseProxy } from 'comlink';
 import { Solitaire } from 'types/game';
 
 export const isTheGameComplete = async (solitaire: Solitaire): Promise<boolean> => {
   const worker = new Worker('workers/isGameCompleteWorker', { name: 'is-game-complete-worker', type: 'module' });
   const api = wrap<import('workers/isGameCompleteWorker').WorkerType>(worker);
-  return await api.isGameComplete(solitaire);
+  const result = await api.isGameComplete(solitaire);
+  api[releaseProxy]();
+  worker.terminate();
+  return result;
 }
 
 export const canCardMove = async (solitaire: Solitaire): Promise<boolean> => {
   const worker = new Worker('workers/canCardMoveWorker', { name: 'can-card-move-worker', type: 'module' });
   const api = wrap<import('workers/canCardMoveWorker').WorkerType>(worker);
-  return await api.canCardMove(solitaire);
+  const result = await api.canCardMove(solitaire);
+  api[releaseProxy]();
+  worker.terminate();
+  return result;
 }
