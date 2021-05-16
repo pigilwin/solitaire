14d8fdb3a535210bd667424488c7a235e4c0b125
Tim Remnant <timwebdesign2013@gmail.com>
2021-05-16 22:29:38 +0100

    Moving choose location to file

--- src/game/board.tsx
+++ src/game/board.tsx
@@ -1,15 +1,10 @@
+import { useSelector } from "react-redux";
+
 import { TopBar } from "./components/board/Top/TopBar";
 import { Columns } from "./components/board/Columns";
-import { useDispatch, useSelector } from "react-redux";
-import { currentGameSelector } from "store/game/gameSlice";
-import { LocationAwareSolitaireCard } from "types/game";
-import { columnFromLocation } from "store/game/builder/util";
-import { Column } from "./components/board/Column";
-import { ColumnContainer } from "./layout/ColumnContainer";
-import { GameButton } from "./components/Button";
-import { makeCardLocationAware } from "store/game/locationHelper";
-import { moveCardToColumnAsync } from "store/game/thunk";
-import { potentialMoveLocationsSelector, cardWantingToBeMovedSelector, clearPossibleMovesAction } from "store/game/gameMoveSlice";
+import { ChooseLocation } from './components/ChooseLocation';
+
+import { potentialMoveLocationsSelector } from "store/game/gameMoveSlice";
 
 export const Board = (): JSX.Element => {
     
@@ -29,59 +24,3 @@ export const Board = (): JSX.Element => {
         </div>
     );
 };
-
-interface ChooseLocationProps {
-    moves: LocationAwareSolitaireCard[];
-}
-const ChooseLocation = ({moves}: ChooseLocationProps): JSX.Element => {
-    const dispatch = useDispatch();
-    
-    const solitaire = useSelector(currentGameSelector);
-    const cardWantingToBeMoved = useSelector(cardWantingToBeMovedSelector);
-
-
-    const columnsBasedOnLocationsOfMoves = moves.map((card) => {
-        return {
-            location: card.location,
-            cards: columnFromLocation(solitaire, card.location.namespace, card.location.area)
-        };
-    });
-
-    const goBackClickHandler = (): void => {
-        dispatch(clearPossibleMovesAction());
-    };
-
-    return (
-        <div>
-            <div className="mx-auto">
-                <h1 className="text-center text-2xl">Choose a column to add the card to.</h1>
-            </div>
-            <ColumnContainer id='location-of-moves'>
-                {columnsBasedOnLocationsOfMoves.map((column, index) => {
-
-                    const chooseColumnClickHandler = (): void => {
-                        const card = makeCardLocationAware(
-                            column.cards[column.cards.length - 1], 
-                            column.location.namespace, 
-                            column.location.area
-                        );
-                        dispatch(moveCardToColumnAsync({
-                            drag: cardWantingToBeMoved,
-                            drop: card
-                        }));
-                        dispatch(clearPossibleMovesAction());
-                    };
-
-                    return (
-                        <div className="cursor-pointer" key={index} onClick={chooseColumnClickHandler}>
-                            <Column cards={column.cards} column={column.location.area}/>
-                        </div>
-                    );
-                })}
-            </ColumnContainer>
-            <div className="flex justify-center items-center">
-                <GameButton buttonText="Back" onClick={goBackClickHandler}/>
-            </div>
-        </div>
-    );
-}
---
+++ src/game/components/ChooseLocation.tsx
@@ -0,0 +1,69 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import { LocationAwareSolitaireCard } from "types/game";
+
+import { currentGameSelector } from "store/game/gameSlice";
+import { columnFromLocation } from "store/game/builder/util";
+import { makeCardLocationAware } from "store/game/locationHelper";
+import { moveCardToColumnAsync } from "store/game/thunk";
+import { cardWantingToBeMovedSelector, clearPossibleMovesAction } from "store/game/gameMoveSlice";
+
+import { Column } from './board/Column';
+import { ColumnContainer } from '../layout/ColumnContainer';
+import { GameButton } from './Button';
+
+interface ChooseLocationProps {
+    moves: LocationAwareSolitaireCard[];
+}
+export const ChooseLocation = ({moves}: ChooseLocationProps): JSX.Element => {
+    const dispatch = useDispatch();
+    
+    const solitaire = useSelector(currentGameSelector);
+    const cardWantingToBeMoved = useSelector(cardWantingToBeMovedSelector);
+
+
+    const columnsBasedOnLocationsOfMoves = moves.map((card) => {
+        return {
+            location: card.location,
+            cards: columnFromLocation(solitaire, card.location.namespace, card.location.area)
+        };
+    });
+
+    const goBackClickHandler = (): void => {
+        dispatch(clearPossibleMovesAction());
+    };
+
+    return (
+        <div>
+            <div className="mx-auto">
+                <h1 className="text-center text-2xl">Choose a column to add the card to.</h1>
+            </div>
+            <ColumnContainer id='location-of-moves'>
+                {columnsBasedOnLocationsOfMoves.map((column, index) => {
+
+                    const chooseColumnClickHandler = (): void => {
+                        const card = makeCardLocationAware(
+                            column.cards[column.cards.length - 1], 
+                            column.location.namespace, 
+                            column.location.area
+                        );
+                        dispatch(moveCardToColumnAsync({
+                            drag: cardWantingToBeMoved,
+                            drop: card
+                        }));
+                        dispatch(clearPossibleMovesAction());
+                    };
+
+                    return (
+                        <div className="cursor-pointer" key={index} onClick={chooseColumnClickHandler}>
+                            <Column cards={column.cards} column={column.location.area}/>
+                        </div>
+                    );
+                })}
+            </ColumnContainer>
+            <div className="flex justify-center items-center">
+                <GameButton buttonText="Back" onClick={goBackClickHandler}/>
+            </div>
+        </div>
+    );
+}
