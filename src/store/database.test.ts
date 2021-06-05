0156e1a387891df4123dad9464e8162c20cd8ba4
Tim Remnant <timwebdesign2013@gmail.com>
2021-06-05 14:47:26 +0100

    Removing tests as cypress code coverage now handles this

--- src/store/database.test.ts
+++
@@ -1,7 +0,0 @@
-import { openDatabase } from './database';
-
-test('Can a database be created', async () => {
-    require("fake-indexeddb/auto");
-    const database = await openDatabase();
-    expect(database).toBeInstanceOf(IDBDatabase);
-});
--- src/store/index.test.ts
+++
@@ -1,5 +0,0 @@
-import {initialiseStore} from './index';
-
-test('should create a store', () => {
-    expect(initialiseStore().getState().gameReducer.game.id).toBe('');
-});
