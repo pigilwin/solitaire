cc8a3d147f3af047e7b11122fa8e06e059207763
Tim Remnant <timwebdesign2013@gmail.com>
2021-06-05 19:29:25 +0100

    Adding leaderboard tests

--- cypress/integration/e2e/leaderboard.spec.ts
+++ cypress/integration/e2e/leaderboard.spec.ts
@@ -1,17 +1,25 @@
 it('should save the game to the leaderboard', () => {
     cy.enableTesting();
 
-    cy.createGameSavedToLeaderboard();
+    cy.createCompletedGameAndSave();
 });
 
 it('should save the game to the leaderboard, visit the leaderboard then go home', () => {
     cy.enableTesting();
 
-    cy.createGameSavedToLeaderboard();
+    cy.createCompletedGameAndSave();
 
     cy.dataCy('cy-leaderboard').click({
         force: true
     });
 
     cy.dataCy('cy-leaderboard-go-home').click();
+});
+
+it('should complete the game the dismiss the save option', () => {
+    cy.enableTesting();
+
+    cy.createCompletedGame();
+
+    cy.dataCy('cy-do-not-save').click();
 });
