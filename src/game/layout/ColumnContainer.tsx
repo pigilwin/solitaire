7690c1d7ecc3f3aa9d7d01b3e4023e3cf6fdb88c
TimRemnant <tremnant@dizions.co.uk>
2021-06-22 15:23:31 +0100

    Card layout in the columns is more mobile friendly

--- src/game/layout/ColumnContainer.tsx
+++ src/game/layout/ColumnContainer.tsx
@@ -6,7 +6,7 @@ interface ColumnContainerProps {
 export const ColumnContainer = ({id, children}: PropsWithChildren<ColumnContainerProps>): JSX.Element => {
     return (
         <div id={id} className="mt-10">
-            <div className="flex flex-row space-x-5 justify-around">
+            <div className="flex flex-row space-x-2 md:space-x-5 justify-around">
                 {children}
             </div>
         </div>
