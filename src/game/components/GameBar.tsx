85d99f7b30fe9a78aba0fb01fc03bd26d6f92130
TimRemnant <tremnant@dizions.co.uk>
2021-06-22 09:32:18 +0100

    GameBar can now open a menu icon

--- src/game/components/GameBar.tsx
+++ src/game/components/GameBar.tsx
@@ -8,11 +8,14 @@ import { MovesCount } from './bar/MovesCount';
 import { UndoGameButton } from './bar/UndoButton';
 import { GameButtons } from "./bar/GameButtons";
 import { ScoreCount } from "./bar/ScoreCount";
+import {useState} from "react";
 
 interface GameBarProps {
     isGameComplete: boolean;
 }
 export const GameBar = ({isGameComplete}: GameBarProps): JSX.Element => {
+
+    const [isMenuOpen, openMenu] = useState(false);
     
     const weHaveHistory = useSelector(doWeHaveAnyHistorySelector);
 
@@ -24,15 +27,47 @@ export const GameBar = ({isGameComplete}: GameBarProps): JSX.Element => {
     const movesCount = useSelector(currentMovesSelector);
     const scoreCount = useSelector(currentScoreSelector);
 
+    const miniMenuClasses: string[] = [
+        'w-full', 'sm:w-auto', 'self-end', 'sm:self-center',
+        'sm:flex', 'flex-col', 'sm:flex-row', 'items-center',
+        'justify-between', 'h-full', 'py-1', 'pb-4',
+        'sm:py-0', 'sm:pb-0'
+    ];
+
+    const openMenuClickHandler = (): void => {
+        openMenu(!isMenuOpen);
+    };
+
+    if (isMenuOpen) {
+        miniMenuClasses.push('flex', 'open');
+    } else {
+        miniMenuClasses.push('hidden');
+    }
+
     return (
-        <nav className="hidden md:block w-full bg-white shadow-lg border-b border-green-500 h-16 mb-2">
-            <div className="container mx-auto px-6 py-3 flex justify-between items-center">
+        <nav className="flex flex-col sm:flex-row w-full justify-between items-center px-4 sm:px-6 py-1 bg-white shadow sm:shadow-none border-t-4 border-red-900">
+            <div className="w-full sm:w-auto self-start sm:self-center flex flex-row sm:flex-none flex-no-wrap justify-between items-center">
                 <GameLogo/>
-                {undoButton}
                 <MovesCount count={movesCount}/>
                 <ScoreCount count={scoreCount}/>
+                <button className="hamburger block sm:hidden focus:outline-none" type="button" onClick={openMenuClickHandler}>
+                    <MenuIcon/>
+                </button>
+            </div>
+            <div className={miniMenuClasses.join(' ')}>
+                {undoButton}
                 <GameButtons/>
             </div>
         </nav>
     );
 };
+
+const MenuIcon = (): JSX.Element => {
+    return (
+        <svg viewBox="0 0 100 80" width="40" height="40">
+            <rect width="100" height="10"/>
+            <rect y="30" width="100" height="10"/>
+            <rect y="60" width="100" height="10"/>
+        </svg>
+    );
+}
--- src/game/components/Loading.tsx
+++ src/game/components/Loading.tsx
@@ -11,13 +11,13 @@ export const Loading = (): JSX.Element => {
 const Rings = (): JSX.Element => {
     return (
         <svg viewBox="0 0 45 45" height="200" width="200" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
-            <g fill="none" fill-rule="evenodd" transform="translate(1 1)" stroke-width="2">
-                <circle cx="22" cy="22" r="6" stroke-opacity="0">
+            <g fill="none" fillRule="evenodd" transform="translate(1 1)" strokeWidth="2">
+                <circle cx="22" cy="22" r="6" strokeOpacity="0">
                     <animate attributeName="r" begin="1.5s" dur="3s" values="6;22" calcMode="linear" repeatCount="indefinite" />
                     <animate attributeName="stroke-opacity" begin="1.5s" dur="3s" values="1;0" calcMode="linear" repeatCount="indefinite" />
                     <animate attributeName="stroke-width" begin="1.5s" dur="3s" values="2;0" calcMode="linear" repeatCount="indefinite" />
                 </circle>
-                <circle cx="22" cy="22" r="6" stroke-opacity="0">
+                <circle cx="22" cy="22" r="6" strokeOpacity="0">
                     <animate attributeName="r" begin="3s" dur="3s" values="6;22" calcMode="linear" repeatCount="indefinite"/>
                     <animate attributeName="stroke-opacity" begin="3s" dur="3s" values="1;0" calcMode="linear" repeatCount="indefinite" />
                     <animate attributeName="stroke-width" begin="3s" dur="3s" values="2;0" calcMode="linear" repeatCount="indefinite" />
