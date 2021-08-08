bff2c4393503608881c755b3fd8afc9f698a32e0
Tim Remnant <timwebdesign2013@gmail.com>
2021-08-08 22:47:22 +0100

    Refactored check login to be boolean, moved to custom method

--- src/workers/canCardMoveWorker.ts
+++ src/workers/canCardMoveWorker.ts
@@ -1,8 +1,8 @@
 import { expose } from "comlink";
-import { enhanceCard, enhanceSolitaire } from "lib/enhancers/enhancers";
 import { columnFromLocation, finalFromLocation, makeCardLocationAware } from "lib/util";
 import { LocationAwareSolitaireCard, Solitaire } from "types/game";
 import { CanCardMoveFromWorker, PotentialClickCardMovesWorker } from "types/worker";
+import { canCardMoveToCard } from "./cardMoves";
 
 const fetchTopLocationAwareCardFromColumns = (
     build: PotentialClickCardMovesWorker,  
@@ -45,8 +45,6 @@ const fetchTopLocationAwareCardFromFinal = (
 };
 
 const canCardMove = (solitaire: Solitaire, card: LocationAwareSolitaireCard): CanCardMoveFromWorker => {
-
-    const enhancedSolitaire = enhanceSolitaire(solitaire);
     const potentialMoves: PotentialClickCardMovesWorker = {};
 
 
@@ -69,104 +67,12 @@ const canCardMove = (solitaire: Solitaire, card: LocationAwareSolitaireCard): Ca
     fetchTopLocationAwareCardFromColumns(potentialMoves, solitaire, 'six');
     fetchTopLocationAwareCardFromColumns(potentialMoves, solitaire, 'seven');
 
-    const enhancedCardToCheck = enhanceCard(card);
-
     for (const key in potentialMoves) {
         const inner = potentialMoves[key];
-        const enhancedInner = enhanceCard(inner);
 
-        /**
-         * If the card we have clicked on is of type king
-         */
-        if (enhancedCardToCheck.isAKing()) {
-
-            /**
-             * If the card we are checking currently on the final stack, is a queen and matches
-             * the suit then allow it to be processed
-             */
-            if (enhancedInner.isAQueen() && enhancedInner.isOnFinal() && enhancedInner.hasIdenticalSuit(card)) {
-                return inner;
-            }
-
-            /**
-             * If the card we are checking is a full card
-             * then don't allow the card to be placed upon
-             */
-            if (!enhancedInner.isAFullCard() && !enhancedInner.isOnFinal()){
-                return inner;
-            }
-
-            continue;
-        }
-
-        /**
-         * If the card we have clicked on is of type ace
-         */
-        if (enhancedCardToCheck.isAAce()) {
-            /**
-             * If the card we are checking is on a final and not a full card then
-             * allow the card to move it to the space
-             */
-            if (enhancedInner.isOnFinal() && !enhancedInner.isAFullCard() && inner.location.area === card.suit.toLowerCase()) {
-                return inner;
-            }
-
-            continue;
-        }
-
-         /**
-         * The current card we are looping over has only a 
-         * location, then this is a empty space. Empty spaces
-         * can be forgotten about as only kings can use these
-         */
-        if (!enhancedInner.isAFullCard()){
-            continue;
-        }
-        
-        const innerAsLocationAware = inner as LocationAwareSolitaireCard;
-
-        /**
-         * If the suits match and the card, the card index
-         * of the card being checked plus one matches the 
-         * card clicked on, both card suits match and the
-         * card being clicke on has no children then the
-         * card can be moved to the final
-         */
-         if (
-            enhancedInner.isOnFinal() && 
-            innerAsLocationAware.index + 1 === card.index && 
-            enhancedInner.hasIdenticalSuit(card) && 
-            !enhancedSolitaire.doAnyCardsExistAsChildren(card)
-        ) {
+        if (canCardMoveToCard(solitaire, card, inner)) {
             return inner;
         }
-
-        /**
-         * If the card indexes aren't compatible then don't allow
-         * the index to be moved
-        */
-         if (innerAsLocationAware.index === card.index + 1 && !enhancedInner.isOnFinal()) {
-            return innerAsLocationAware;
-        }
-
-        /**
-         * If the cards are indentical then it 
-         * can't be moved to this stack
-        */
-         if (enhancedInner.isIdenticalToo(card)) {
-            continue;
-        }
-
-        /**
-         * If the cards are of the same color then 
-         * these can't be transferred
-        */
-        if (enhancedInner.hasIdenticalColour(card)) {
-            continue;
-        }
-
-        continue;
-
     }
 
     return undefined;
---
+++ src/workers/cardMoves.ts
@@ -0,0 +1,92 @@
+import { enhanceCard, enhanceSolitaire } from "lib/enhancers/enhancers";
+import { LocationAwarePotentiallyUndefinedSolitaireCard, LocationAwareSolitaireCard, Solitaire } from "types/game";
+
+
+
+export const canCardMoveToCard = (
+    solitaire: Solitaire, 
+    card: LocationAwareSolitaireCard, 
+    inner: LocationAwarePotentiallyUndefinedSolitaireCard
+): boolean => {
+    const enhancedSolitaire = enhanceSolitaire(solitaire);
+    const enhancedCardToCheck = enhanceCard(card);
+    const enhancedInner = enhanceCard(inner);
+
+
+    /**
+     * If the card we have clicked on is of type king
+     */
+     if (enhancedCardToCheck.isAKing()) {
+
+        /**
+         * If the card we are checking currently on the final stack, is a queen and matches
+         * the suit then allow it to be processed
+         */
+        if (enhancedInner.isAQueen() && enhancedInner.isOnFinal() && enhancedInner.hasIdenticalSuit(card)) {
+            return true;
+        }
+
+        /**
+         * If the card we are checking is a full card
+         * then don't allow the card to be placed upon
+         */
+        if (!enhancedInner.isAFullCard() && !enhancedInner.isOnFinal()){
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * If the card we have clicked on is of type ace
+     */
+    if (enhancedCardToCheck.isAAce()) {
+        /**
+         * If the card we are checking is on a final and not a full card then
+         * allow the card to move it to the space
+         */
+        if (enhancedInner.isOnFinal() && !enhancedInner.isAFullCard() && inner.location.area === card.suit.toLowerCase()) {
+            return true;
+        }
+
+        return false;
+    }
+
+     /**
+     * The current card we are looping over has only a 
+     * location, then this is a empty space. Empty spaces
+     * can be forgotten about as only kings can use these
+     */
+    if (!enhancedInner.isAFullCard()){
+        return false;
+    }
+    
+    const innerAsLocationAware = inner as LocationAwareSolitaireCard;
+    const cardAsLocationAware = card as LocationAwareSolitaireCard;
+
+    /**
+     * If the suits match and the card, the card index
+     * of the card being checked plus one matches the 
+     * card clicked on, both card suits match and the
+     * card being clicke on has no children then the
+     * card can be moved to the final
+     */
+     if (
+        enhancedInner.isOnFinal() && 
+        innerAsLocationAware.index + 1 === cardAsLocationAware.index && 
+        enhancedInner.hasIdenticalSuit(cardAsLocationAware) && 
+        !enhancedSolitaire.doAnyCardsExistAsChildren(cardAsLocationAware)
+    ) {
+        return true;
+    }
+
+    /**
+     * If the card indexes aren't compatible then don't allow
+     * the index to be moved
+    */
+     if (innerAsLocationAware.index === cardAsLocationAware.index + 1 && !enhancedInner.isOnFinal()) {
+        return true;
+    }
+
+    return false;
+};
